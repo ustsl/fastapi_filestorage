f96233deee851959103948584c0cb4a6bca82e54
ustsl <w@imvo.site>
2024-06-06 21:54:37 +0300

    upd

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from uvicorn import Config as UvicornConfig, Server
 
 
 # Запуск Uvicorn
-config = UvicornConfig("src.main:app", host="127.0.5.9", port=52608, reload=True)
+config = UvicornConfig("src.main:app", host="127.0.5.9", port=51593, reload=True)
 server = Server(config)
 
 if __name__ == "__main__":
